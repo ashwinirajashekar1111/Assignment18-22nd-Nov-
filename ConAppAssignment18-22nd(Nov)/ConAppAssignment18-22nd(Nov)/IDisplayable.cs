8c3047ed6f62dde9a0cb0f4b8be0507e36c49b07
AshwiniRajashekar <ashwinirajashekar1111@gmail.com>
2023-11-22 16:09:42 +0530

    Assignment17-21st(Nov)

---
+++ ConAppAssignment18-22nd(Nov)/ConAppAssignment18-22nd(Nov)/IDisplayable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment18_22nd_Nov_
+{
+    public interface IDisplayable
+    {
+        void Display();
+    }
+}
---
+++ ConAppAssignment18-22nd(Nov)/ConAppAssignment18-22nd(Nov)/IRecharable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment18_22nd_Nov_
+{
+    internal interface IRecharable
+    {
+        void charge(int minute);
+    }
+}
